a0012b17e7cbce061b331c766d8bd90e2d2bf637
tpmacc <tpmacc@yahoo.com>
2025-06-05 04:01:31 -0400

    add hello visitor endpoint

--- src/main/java/tpmacc/demo/api/DemoController.java
+++ src/main/java/tpmacc/demo/api/DemoController.java
@@ -1,6 +1,8 @@
 package tpmacc.demo.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -12,4 +14,10 @@ public class DemoController {
      public Map<String, String> hello() {
          return Map.of("message", "Hello, World!");
      }
+
+     @GetMapping("/hello/{name}")
+     public Map<String, String> hello(@PathVariable(value = "name") String name) {
+         return Map.of("message", "Hello, %s!".formatted(name));
+     }
+
 }
